b603b5feaa02079f98f6abdc5d5455816c3ebc70
Stijn Van Hulle <stijn@stijnvanhulle.be>
2023-06-17 17:44:31 +0200

    chore: update packages for kubb 1.x.x

--- src/plugin.ts
+++ src/plugin.ts
@@ -1,78 +1,86 @@
-/* eslint-disable @typescript-eslint/ban-types */
-/* eslint-disable @typescript-eslint/no-unused-vars */
-/* eslint-disable no-console */
+import pathParser from 'node:path'
 
-import pathParser from 'path'
-
-import { createPlugin, getPathMode, validatePlugins } from '@kubb/core'
+import { createPlugin, getPathMode, validatePlugins, writeIndexes } from '@kubb/core'
 import { pluginName as swaggerPluginName } from '@kubb/swagger'
-import { writeIndexes } from '@kubb/ts-codegen'
-import type { Api as SwaggerApi } from '@kubb/swagger'
 
-import type { Api } from '.'
-import type { PluginOptions } from './types'
+import { camelCase, camelCaseTransformMerge } from 'change-case'
 
-export const pluginName = 'plugin-demo' as const
+import type { File } from '@kubb/core'
+import type { API as SwaggerApi } from '@kubb/swagger'
+import type { PluginOptions } from './types.ts'
+
+export const pluginName: PluginOptions['name'] = 'plugin-demo' as const
+
+// Register your plugin for maximum type safety
+declare module '@kubb/core' {
+  interface Register {
+    ['@kubb/plugin-demo']: PluginOptions['options']
+  }
+}
 
 export const definePlugin = createPlugin<PluginOptions>((options) => {
-  const { output = 'output' } = options
+  const { output = 'demo' } = options
   let swaggerApi: SwaggerApi
 
-  const api: Api = {
-    resolveId(fileName, directory) {
-      if (!directory) {
-        return null
+  return {
+    name: pluginName,
+    options,
+    kind: 'controller',
+    validate(plugins) {
+      const valid = validatePlugins(plugins, [swaggerPluginName])
+      if (valid) {
+        swaggerApi = plugins.find((plugin) => plugin.name === swaggerPluginName)?.api as SwaggerApi
       }
 
-      const mode = getPathMode(pathParser.resolve(directory, output))
+      return valid
+    },
+    resolvePath(fileName, directory, options) {
+      const root = pathParser.resolve(this.config.root, this.config.output.path)
+      const mode = getPathMode(pathParser.resolve(root, output))
 
       if (mode === 'file') {
         /**
          * when output is a file then we will always append to the same file(output file), see fileManager.addOrAppend
          * Other plugins then need to call addOrAppend instead of just add from the fileManager class
          */
-        return pathParser.resolve(directory, output)
+        return pathParser.resolve(root, output)
       }
 
-      return pathParser.resolve(directory, output, fileName)
+      return pathParser.resolve(root, output, fileName)
     },
-  }
+    resolveName(name) {
+      return camelCase(name, { delimiter: '', stripRegexp: /[^A-Z0-9$]/gi, transform: camelCaseTransformMerge })
+    },
+    async buildStart() {
+      // const oas = await swaggerApi.getOas()
 
-  return {
-    name: pluginName,
-    options,
-    kind: 'schema',
-    api,
-    validate(plugins) {
-      const valid = validatePlugins(plugins, [swaggerPluginName])
-      if (valid) {
-        swaggerApi = plugins.find((plugin) => plugin.name === swaggerPluginName)?.api
-      }
+      const files: File[] = [
+        {
+          fileName: 'test.ts',
+          path: this.resolvePath({
+            fileName: 'test.ts',
+            pluginName,
+          })!,
+          source: "export const hello = 'world';",
+        },
+      ]
+      await this.addFile(...files)
 
-      return valid
+      console.log('Build start')
     },
-    resolveId(fileName, directory) {
-      return api.resolveId(fileName, directory)
-    },
-    async writeFile(source, path) {
-      if (!path.endsWith('.ts') || !source) {
+    async buildEnd() {
+      if (this.config.output.write === false) {
         return
       }
 
-      await this.fileManager.write(source, path)
-    },
-    async buildStart() {
-      const oas = await swaggerApi.getOas(this.config)
-      const schemas = oas.getDefinition().components?.schemas || {}
-      const directory = pathParser.resolve(this.config.root, this.config.output.path)
-      const mode = getPathMode(pathParser.resolve(directory, output))
+      const root = pathParser.resolve(this.config.root, this.config.output.path)
+      const files = await writeIndexes(root, { extensions: /\.ts/, exclude: [/schemas/, /json/] })
 
-      console.log('Build')
-    },
-    async buildEnd() {
-      if (this.config.output.write || this.config.output.write === undefined) {
-        await writeIndexes(this.config.root, this.config.output.path, { extensions: /\.ts/, exclude: [/schemas/, /json/] })
+      if (files) {
+        await this.addFile(...files)
       }
+
+      console.log('Build end')
     },
   }
 })
--- src/types.ts
+++ src/types.ts
@@ -1,11 +1,11 @@
 import type { PluginFactoryOptions } from '@kubb/core'
 
 export type Api = {
-  resolveId: (fileName: string, directory: string | undefined) => string | null
+  getDemo: () => string | null
 }
 
 export type Options = {
   output?: string
 }
 
-export type PluginOptions = PluginFactoryOptions<Options, false, Api>
+export type PluginOptions = PluginFactoryOptions<'plugin-demo', Options, false, Api>
